6f9ca0b725e795cf28b0ecef361c10171be329b6
Taishawn King <taishawnking1@yahoo.com>
2020-01-25 03:16:06 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,11 @@
+require"pry"
+class ChattyStudent < Student 
+def  hello
+ 2.times {super}
+  puts "How are you doing today? I'm okay, but I'm kind of tire. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    
+end
+def raise_hand
+ 10.times{super}
+ end
+end
